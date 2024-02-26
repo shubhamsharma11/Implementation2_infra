50bc57fae9d493538452bb4b0dfa2d5eeec9fc32
Shub <s@gmail.com>
2024-02-26 17:08:20 +0000

    updated tf file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -122,8 +122,8 @@ resource "azurerm_virtual_machine" "vm01" {
     }
     os_profile {
         computer_name = "hostname"
-        admin_username = "testadmin"
-        admin_password = "Password1234!"
+        admin_username = var.connection["username"]
+        admin_password = var.connection["password"]
     }
     os_profile_linux_config {
         disable_password_authentication = false
