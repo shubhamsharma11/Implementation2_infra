bdc28f1142b5969bc4e22495273fd9f093b0c138
Shub <s@gmail.com>
2024-02-27 09:16:42 +0000

    updated tf and ansible file

--- AgentTf/main.tf
+++ AgentTf/main.tf
@@ -106,7 +106,7 @@ resource "null_resource" "execute_script" {
       "mkdir myagent && cd myagent",
       "wget -O vsts-agent-linux-x64-3.234.0.tar.gz https://vstsagentpackage.azureedge.net/agent/3.234.0/vsts-agent-linux-x64-3.234.0.tar.gz",
       "tar zxvf vsts-agent-linux-x64-3.234.0.tar.gz",
-      "./config.sh --unattended --url https://dev.azure.com/Shubham1708698304552/ --auth pat --token x4x4wzwy7w53ikj54ipzhbnmjjnfohcewotpag7zx27ugkgusqmq --pool Default --agent LinuxAgent01 --acceptTeeEula",
+      "./config.sh --unattended --url https://dev.azure.com/Shubham1708698304552/ --auth pat --token x4x4wzwy7w53ikj54ipzhbnmjjnfohcewotpag7zx27ugkgusqmq --pool Default --agent LinuxAgent01 --acceptTeeEula --replace",
       "./run.sh"
     ]
 
