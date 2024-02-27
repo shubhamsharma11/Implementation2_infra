5ef7639800fe49b9b16dd902bfdfd243f125df14
Shub <s@gmail.com>
2024-02-27 13:01:57 +0000

    updated script file

--- Terraform/script.sh
+++ Terraform/script.sh
@@ -18,5 +18,7 @@ ansible --version
 echo "Ansible has been successfully installed."
 
 cd /tmp
+ls
+cat inventory
 ansible-playbook -i inventory deploy.yaml --ssh-extra-args='-o StrictHostKeyChecking=no'
 echo "Ansible Playbook Executed Successully !!!"
