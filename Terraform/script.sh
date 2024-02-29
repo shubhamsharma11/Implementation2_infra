3e01311bd5d726980584f5eea32477befa1f9fd6
Shub <s@gmail.com>
2024-02-29 06:59:52 +0000

    updated the yaml file to run agent

--- Terraform/script.sh
+++ Terraform/script.sh
@@ -21,3 +21,12 @@ cd /tmp
 ls
 ansible-playbook -i localhost deploy.yaml --ssh-extra-args='-o StrictHostKeyChecking=no'
 echo "Ansible Playbook Executed Successully !!!"
+
+echo "Azure self hosted agent installation started."
+mkdir myagent && cd myagent
+wget -O vsts-agent-linux-x64-3.234.0.tar.gz https://vstsagentpackage.azureedge.net/agent/3.234.0/vsts-agent-linux-x64-3.234.0.tar.gz
+tar zxvf vsts-agent-linux-x64-3.234.0.tar.gz
+./config.sh --unattended --url https://dev.azure.com/Shubham1708698304552/ --auth pat --token x4x4wzwy7w53ikj54ipzhbnmjjnfohcewotpag7zx27ugkgusqmq --pool Default --agent LinuxAgent01 --acceptTeeEula --replace
+echo "Azure self hosted agent installation successful."
+echo "Azure self hosted agent started running !!!"
+./run.sh
